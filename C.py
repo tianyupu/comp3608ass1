c5685145a83918eece7ca1a4d6d898357ab76b64
Lisa Fedorenko <lisafedorenko@gmail.com>
2012-04-20 12:30:45 +1000

    integrated some A changes to B thought about C

--- C.py
+++ C.py
@@ -3,3 +3,6 @@
 # Alpha-beta pruning. But store best 3 solutions, then go an extra couple of layers deep to explore the best solution of those
 
 # unless we come up with something else
+
+# or just alpha-beta pruning with a random elemnt
+import random
